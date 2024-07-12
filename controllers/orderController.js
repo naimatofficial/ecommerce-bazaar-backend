1cfa25e6cb9405e27f6e1ed3c7dd447bde1f77e2
Naimat Ullah <naimat.official.10@gmail.com>
2024-07-12 11:17:46 +0500

    order controller and routes done

---
+++ controllers/orderController.js
@@ -0,0 +1,23 @@
+import Order from "../models/orderModel";
+import catchAsync from "../utils/catchAsync";
+
+export const createOrder = catchAsync(async (req, res) => {
+	const customer = req.user?._id;
+
+	const newOrder = {
+		customer,
+		...req.body,
+	};
+
+	const doc = await Order.create(newOrder);
+
+	res.status(201).json({
+		status: "success",
+		doc,
+	});
+});
+
+export const getOrders = getAll(Order);
+export const getOrder = getOne(Order);
+export const deleteOrder = deleteOne(Order);
+export const updateOrder = updateOne(Order);
